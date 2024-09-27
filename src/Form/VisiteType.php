8eab8c2be1b447fe0324c3bc59d3af1d82a815b3
Moi <Moi@Gaby>
2024-09-27 22:57:20 +0200

    phase 13 : ORM (ajout d'une visite)

--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use DateTime;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class VisiteType extends AbstractType
 {
@@ -15,8 +17,11 @@ class VisiteType extends AbstractType
         $builder
             ->add('ville')
             ->add('pays')
-            ->add('datecreation', null, [
-                'widget' => 'single_text', 'label' => 'date'
+            ->add('datecreation', DateType::class, [
+                'widget' => 'single_text', 
+                'data' => isset($options['data']) && 
+                    $options['data']->getDateCreation() != null ? $options['data']->getDateCreation(): new DateTime('now'),
+                'label' => 'date'
             ])
             ->add('note')
             ->add('avis')
